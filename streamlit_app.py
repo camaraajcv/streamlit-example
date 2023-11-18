c11361fafbfda0566d7ba0e9a1568e0323728423
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2023-11-18 20:25:04 +0000

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -104,26 +104,6 @@ def processar_pdf(pdf_content):
     if submit_button:
         exportar_xml(df_final, numero_ne, numero_sb, cpf_responsavel,data_previsao_pagamento)
 
-
-# URL da imagem
-image_url = "https://www.fab.mil.br/om/logo/mini/dirad2.jpg"
-
-#Código HTML e CSS para ajustar a largura da imagem para 20% da largura da coluna e centralizar
-html_code = f'<div style="display: flex; justify-content: center;"><img src="{image_url}" alt="Imagem" style="width:8vw;"/></div>'
-
-
-# Exibir a imagem usando HTML
-st.markdown(html_code, unsafe_allow_html=True)
-
-
-# Centralizar o texto abaixo da imagem
-st.markdown("<h1 style='text-align: center; font-size: 1.5em;'>DIRETORIA DE ADMINISTRAÇÃO DA AERONÁUTICA</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center; font-size: 1.2em;'>SUBDIRETORIA DE PAGAMENTO DE PESSOAL</h2>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center; font-size: 1em; text-decoration: underline;'>PP1 - DIVISÃO DE DESCONTOS</h3>", unsafe_allow_html=True)
-
-# Texto explicativo
-st.write("Desconto Externo Civil - Extração dados PDF SIAPE para SIAFI")
-
 # Função para processar o PDF e exibir o resultado
 def processar_pdf(pdf_content):
     global ultimo_sequencial, data_geracao  # Declare as variables as global
