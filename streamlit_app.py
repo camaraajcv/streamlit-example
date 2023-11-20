4906436ae79570a9b9573da69ae9d4cfd48da3a1
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2023-11-20 19:39:56 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import tempfile
 import streamlit as st
 import os
 from datetime import datetime
+import regex as re
+
 # URL da imagem
 image_url = "https://www.fab.mil.br/om/logo/mini/dirad2.jpg"
 
