bc512513ed2baaf2b86e6ac9737793e9fcf3328a
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2023-12-07 16:13:11 +0000

    ddd

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ image_url = "https://www.fab.mil.br/om/logo/mini/dirad2.jpg"
 #Código HTML e CSS para ajustar a largura da imagem para 20% da largura da coluna e centralizar
 html_code = f'<div style="display: flex; justify-content: center;"><img src="{image_url}" alt="Imagem" style="width:8vw;"/></div>'
 
-data_geracao = datetime.now().strftime('%Y-%m-%d')
-data_geracao2 = datetime.now().strftime('%d-%m-%Y')
+data_geracao = datetime.now().strftime('%Y/%m/%d')
+data_geracao2 = datetime.now().strftime('%d/%m/%Y')
 
 
 # Exibir a imagem usando HTML
