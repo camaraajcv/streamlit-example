f8e8e41125e25749c5a7de7a735199bfe098ce82
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2023-12-08 15:03:11 +0000

    ss

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,7 +116,7 @@ def processar_pdf(pdf_content):
     valores_para_filtrar = ['34685', '34447', '30846']
     
     # Filtrar o DataFrame
-    df_rubricas_excluidas = df[df['Rubrica'].isin(valores_para_filtrar)][['Rubrica', 'Valor Líquido']]
+    df_rubricas_excluidas = df[df['Rubrica'].isin(valores_para_filtrar)][['Rubrica','Empresa','Valor Líquido']]
     df_final=df.drop('Texto_Após_CNPJ', axis=1)
 
     # Obter o índice da primeira linha do DataFrame df_final
