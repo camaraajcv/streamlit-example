8fc1d601294b83a908bb95ecb568cef4fdbf6f8f
camaraajcv <123605493+camaraajcv@users.noreply.github.com>
2024-11-29 16:24:33 +0000

    asdasdas

--- pages/Desconto Externo Militar.py
+++ pages/Desconto Externo Militar.py
@@ -3,7 +3,7 @@ from PyPDF2 import PdfReader
 import pandas as pd
 import re
 
-# Função para processar o PDF e extrair CNPJs, Conta Corrente e Agência
+# Função para processar o PDF e extrair CNPJs e Agências
 def processar_pdf(file):
     # Ler o conteúdo do PDF
     pdf_reader = PdfReader(file)
@@ -34,28 +34,23 @@ def processar_pdf(file):
     # Encontrar todas as agências entre "Agência:" e "Conta Corrente:"
     agencia_matches = re.findall(agencia_pattern, texto_completo)
 
-    # Garantir que o número de agências seja igual ao número de CNPJs
-    # Se o número de agências for menor, preencher com None (ou NaN)
-    if len(agencia_matches) < len(cnpj_matches):
-        agencia_matches.extend([None] * (len(cnpj_matches) - len(agencia_matches)))
+    # Verificar se o número de agências é menor que o número de CNPJs e preencher com None se necessário
+    while len(agencia_matches) < len(cnpj_matches):
+        agencia_matches.append(None)
 
-    # Criar o DataFrame com os CNPJs, Conta Corrente e Agência encontrados
-    df = pd.DataFrame({
-        "Conta Corrente": conta_corrente_matches,
+    # Criar o DataFrame com os CNPJs e Agência encontrados
+    df_agencia_cnpj = pd.DataFrame({
         "CNPJ": cnpj_matches,
         "Agência": agencia_matches
     })
 
-    # Excluir as linhas onde a "Conta Corrente" começa com "-"
-    df = df[~df['Conta Corrente'].str.startswith('-')]
-
     # Excluir as linhas duplicadas com base no "CNPJ"
-    df = df.drop_duplicates(subset="CNPJ")
+    df_agencia_cnpj = df_agencia_cnpj.drop_duplicates(subset="CNPJ")
 
-    return df
+    return df_agencia_cnpj
 
 # Interface no Streamlit
-st.title("Extração de CNPJ, Conta Corrente e Agência do PDF")
+st.title("Extração de CNPJ e Agência do PDF")
 uploaded_file = st.file_uploader("Faça o upload do arquivo PDF", type="pdf")
 
 if uploaded_file is not None:
@@ -63,7 +58,7 @@ if uploaded_file is not None:
     
     # Processar o PDF e exibir os resultados
     df_resultado = processar_pdf(uploaded_file)
-    st.write("### CNPJs, Conta Corrente e Agência Extraídos:")
+    st.write("### CNPJs e Agências Extraídos:")
     st.dataframe(df_resultado)
 
     # Adicionar opção de download para o DataFrame em CSV
@@ -71,9 +66,10 @@ if uploaded_file is not None:
     st.download_button(
         label="Baixar resultados em CSV",
         data=csv,
-        file_name="cnpj_conta_corrente_agencia_extraidos.csv",
+        file_name="cnpj_agencia_extraidos.csv",
         mime="text/csv",
     )
+
 
 
 
